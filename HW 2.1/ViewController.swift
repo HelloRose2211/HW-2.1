11991f8a98a8405a3eff813c7c5096f4aab29a44
Rose Khaldeeva <khalrose@mail.ru>
2022-05-18 21:51:08 +0900

    Work with view controller

--- HW 2.1/ViewController.swift
+++ HW 2.1/ViewController.swift
@@ -9,18 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
     @IBOutlet weak var redColorView: UIView!
     @IBOutlet weak var yellowColorView: UIView!
     @IBOutlet weak var greenColorView: UIView!
     
-    @IBOutlet weak var pressStartButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        startButton.layer.cornerRadius = 10
+        redColorView.layer.cornerRadius = redColorView.frame.width / 2
+        yellowColorView.layer.cornerRadius = yellowColorView.frame.width / 2
+        greenColorView.layer.cornerRadius = greenColorView.frame.width / 2
         
     }
+    
     @IBAction func pressedStartToNext() {
         
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        
     }
+    
 }
 
