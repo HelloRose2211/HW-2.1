232ca2bb6b991dd7ab6d2dc5ca5e7bb7651da069
Rose Khaldeeva <khalrose@mail.ru>
2022-05-20 13:30:07 +0900

    Work with ViewController

--- HW 2.1/ViewController.swift
+++ HW 2.1/ViewController.swift
@@ -7,25 +7,40 @@
 
 import UIKit
 
+enum CurrentLights {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
-   
     @IBOutlet weak var redColorView: UIView!
     @IBOutlet weak var yellowColorView: UIView!
     @IBOutlet weak var greenColorView: UIView!
     
     @IBOutlet weak var startButton: UIButton!
     
+    private var currentLight = CurrentLights.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         startButton.layer.cornerRadius = 10
         
+        redColorView.alpha = lightIsOff
+        yellowColorView.alpha = lightIsOff
+        greenColorView.alpha = lightIsOff
+        
+        print("Размер стороны, доступный в методе viewDidLoad: \(redColorView.frame.height)")
     }
     
     override func viewDidLayoutSubviews() {
-        redColorView.layer.cornerRadius = redColorView.layer.frame.width / 2
-        yellowColorView.layer.cornerRadius = yellowColorView.layer.frame.width / 2
-        greenColorView.layer.cornerRadius = greenColorView.layer.frame.width / 2
+        redColorView.layer.cornerRadius = redColorView.frame.width / 2
+        yellowColorView.layer.cornerRadius = yellowColorView.frame.width / 2
+        greenColorView.layer.cornerRadius = greenColorView.frame.width / 2
+        
+        print("Размер стороны, доступный в методе viewDidLayoutSubviews: \(redColorView.frame.height)")
     }
     @IBAction func pressedStartToNext() {
         
@@ -33,7 +48,20 @@ class ViewController: UIViewController {
             startButton.setTitle("NEXT", for: .normal)
         }
         
-        
+        switch currentLight {
+        case .red:
+            greenColorView.alpha = lightIsOff
+            redColorView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redColorView.alpha = lightIsOff
+            yellowColorView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenColorView.alpha = lightIsOn
+            yellowColorView.alpha = lightIsOff
+            currentLight = .red
+        }
     }
     
 }
